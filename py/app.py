6ee85b940fcddcca1eb0f72a64149998c3b2e31b
mnolaya <michael.n.olaya@gmail.com>
2024-04-27 05:03:45 -0400

    wip

--- py/app.py
+++ py/app.py
@@ -1,7 +1,7 @@
 import re
 import io
 
-from dash import Dash, dcc
+from dash import Dash, dcc, Input, Output, callback
 import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 
@@ -30,10 +30,35 @@ fig.update_layout(
     yaxis_title='water height [m]'
 )
 
+slider_marks = {time: '' for time in df.select(pl.col('time')).to_series()}
+max_time = df.select(pl.col('time')).max().item()
+min_time = df.select(pl.col('time')).min().item()
+
+def create_fig(time: float = 1) -> go.Figure:
+    return go.Figure(
+        data=[
+            go.Scatter(
+                x=[i for i in range(len(df.columns)-1)], 
+                y=df.select(df.filter(pl.col('time') == time)).select(pl.exclude('time')).to_numpy()[0, :],
+            )
+        ]
+    )
+
+@callback(
+    Output('fig', 'figure'),
+    Input('slider', 'value')
+)
+def set_fig_time(time):
+    print(time)
+    return create_fig(time)
+
 app.layout = dbc.Container(
-    dbc.Col(
-        dbc.Row(
-            dcc.Graph(figure=fig)
+    dbc.Row(
+        dbc.Col(
+            [
+                dcc.Graph(id='fig', figure=fig),
+                dcc.Slider(id='slider', value=min_time, min=min_time, max=max_time, marks=slider_marks)
+            ]
         )
     )
 )
